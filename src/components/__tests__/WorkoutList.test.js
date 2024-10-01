162324f4293a10981754331a52d797f6bfafce19
jnnilsson <jn.jensnilsson@gmail.com>
2024-10-01 15:15:54 +0200

    remove unnecessery props, rename testfile

--- src/components/__tests__/WorkoutList.test.js
+++ src/components/__tests__/WorkoutList.test.js
@@ -3,21 +3,24 @@ import { mount } from "@vue/test-utils";
 import WorkoutList from "@/components/WorkoutList.vue";
 
 test("removeExercise removes the correct exercise", async () => {
-  const wrapper = mount(WorkoutList, {
-    props: {
-      title: "Test Workout Plan",
-    },
-  });
+  const wrapper = mount(WorkoutList);
 
   const exercisesBefore = wrapper.findAll("li");
   expect(exercisesBefore.length).toBe(2);
 
   const removeButton = exercisesBefore[1].find("button");
-
   await removeButton.trigger("click");
 
   const exercisesAfter = wrapper.findAll("li");
   expect(exercisesAfter.length).toBe(1);
   expect(exercisesAfter[0].text()).toContain("Barbell Bench Press");
+
+});
+
+test("addExercise adds a new exercise", async () => {
+  const wrapper = mount(WorkoutList);
+
+
+
 
 });
